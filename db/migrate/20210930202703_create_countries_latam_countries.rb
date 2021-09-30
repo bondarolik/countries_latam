910bee7d603c3dac47383abbdcd908dbe0395375
V. Bruk <vbruk@hey.com>
2021-09-30 17:35:12 -0300

    Update countries migration file

--- db/migrate/20210930202703_create_countries_latam_countries.rb
+++ db/migrate/20210930202703_create_countries_latam_countries.rb
@@ -1,6 +1,8 @@
 class CreateCountriesLatamCountries < ActiveRecord::Migration[6.1]
   def change
     create_table :countries_latam_countries do |t|
+      t.string :name
+      t.string :iso_code
 
       t.timestamps
     end
